65c92264204ba2f167d74e0fbfe6eaffdfa12533
olumide-AI <olumidekolawole26@gmail.com>
2024-12-19 04:51:00 -0800

    cat_detector exercise

--- 2_predictive_stepping/exercises/1_variables/cat_detector.py
+++ 2_predictive_stepping/exercises/1_variables/cat_detector.py
@@ -14,3 +14,12 @@ is_cat = maybe_cat == "cat"
 assert is_cat, 'you should have entered "cat"'
 
 print("thank you for the cat")
+
+""" Started my breakpoint on line 10
+Nothing changes in memory, you're prompted to input a something 
+Whatever you input is now saved in memory under variable maybe_cat
+is_cat = maybe_cat and if variable is == cat, its true and saves in memory
+it runs the print statement and nothing changes in memory
+If not it raises an assertion error and prints an assertion error
+
+"""
